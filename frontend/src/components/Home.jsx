1ee2a884c8406270538876ccc7ef1bfd6d1317f5
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-09-08 21:40:55 +0100

    Add files via upload

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HairCut from './assets/haircut.jpg'
+
+function Home() {
+  return (
+    <div className='z-90  max-w-fit h-full'>
+      <figure>
+        <img className='' src={HairCut} alt='Shoes' />
+      </figure>
+    </div>
+  )
+}
+
+export default Home
