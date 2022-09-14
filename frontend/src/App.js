44b9679ef301628b5252edfb94bf6822cdc316b6
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-09-14 19:10:53 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import BestBarber from './components/BestBarber'
 import Home from './components/Home'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
-
+import Popular from './components/Popular'
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <main className='container  mx-auto bg-red-200 text-red-900'>
         <Home />
         <BestBarber />
+        <Popular />
         container
       </main>
       <Footer />
