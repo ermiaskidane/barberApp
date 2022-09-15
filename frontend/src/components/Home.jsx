2c91679a6aa31b2456caeadfddaaebc5b1e24932
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-09-15 22:00:00 +0100

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import HairCut from './assets/haircut.jpg'
+import HairCut from '../../assets/haircut.jpg'
 
 function Home() {
   return (
-    <div className='z-80 w-full h-screen  '>
-      <figure className='w-full h-[700px] bg-gray-900/70 absolute  '>
+    <div className='z-80 w-full h-[700px] mx-auto relative '>
+      <figure className='w-full h-[700px] bg-gray-900/70   '>
         <img
           className='w-full h-full object-cover mix-blend-overlay '
           src={HairCut}
           alt='Shoes'
         />
       </figure>
-      <div className='max-w-[1240px] mx-auto text-white relative'>
+      <div className=' max-w-[1240px] mx-auto text-white absolute top-0'>
         <div className='flex flex-col justify-items-center  px-4 py-36 lg:py-60'>
           <div className='pb-10'>
             <p className='text-center text-lg uppercase font-light pb-2'>
               Discover Your New barber & Book your Appointment
             </p>
-            <h3 className='text-center text-5xl uppercase font-light'>
+            <h3 className='text-center text-3xl uppercase font-light lg:text-5xl'>
               Making you Look Awesome & Manly{' '}
             </h3>
           </div>
@@ -25,24 +25,24 @@ function Home() {
             <input
               type='text'
               placeholder='select service'
-              className='input input-bordered w-1/2 lg:w-full  lg:mr-2 text-black '
+              className='input input-bordered w-5/6  text-black sm:w-1/2 lg:w-full  lg:mr-2  '
             />
             <input
               type='text'
               placeholder='select country'
-              className='input input-bordered  w-1/2 lg:w-full  lg:mr-2 text-black'
+              className='input input-bordered w-5/6 text-black sm:w-1/2 lg:w-full  lg:mr-2 '
             />
             <input
               type='text'
               placeholder='select state'
-              className='input input-bordered  w-1/2 lg:w-full  lg:mr-2 text-black'
+              className='input input-bordered  w-5/6 text-black sm:w-1/2 lg:w-full  lg:mr-2 '
             />
             <input
               type='text'
               placeholder='select city'
-              className='input input-bordered  w-1/2 lg:w-full  lg:mr-2 text-black'
+              className='input input-bordered  w-5/6 text-black sm:w-1/2 lg:w-full  lg:mr-2 '
             />
-            <button className='bg-indigo-600 px-5 py-3 rounded-lg  '>
+            <button className='bg-indigo-600 px-5 py-3 rounded-lg w-1/2 sm:w-auto '>
               Submit
             </button>
           </form>
@@ -53,4 +53,3 @@ function Home() {
 }
 
 export default Home
-
